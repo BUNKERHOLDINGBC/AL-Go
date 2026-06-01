a8b0ddeb398bdb0a50d796ece314c56b4f0d6893
Freddy Kristiansen <frdk.frd@comp-host.com>
2026-06-01 11:39:13 +0200

    dump projectsorder

--- Actions/AL-Go-Helper.ps1
+++ Actions/AL-Go-Helper.ps1
@@ -2089,6 +2089,7 @@ Function AnalyzeProjectDependencies {
             foreach($dependency in $dependencies) {
                 # Check whether dependency is already resolved by a previous build project
                 Write-Host "Check if $dependency is already resolved by a previous build project"
+                Write-Host "$($projectsOrder | ConvertTo-Json -Depth 10)"
                 $depProject = @($projectsOrder | ForEach-Object { $_.Projects | Where-Object { if ($_ -ne $project) { Write-Host "Dependencies for $project"; foreach($app in $appDependencies."$_".apps) { Write-Host "- $app" } }; $_ -ne $project -and $appDependencies."$_".apps -contains $dependency } })
                 if ($depProject.Count -gt 0) {
                     Write-Host "It is!"
